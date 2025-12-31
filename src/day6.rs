ef884d9c7084f68c6b50639ab50f8de19ae24159
Sarang Joshi <sarangj@msn.com>
2025-12-31 02:45:47 -0600

    Finishd day 6

--- src/day6.rs
+++ src/day6.rs
@@ -1,5 +1,5 @@
 use std::{
-    cmp::min,
+    cmp,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -19,9 +19,9 @@ impl Operator {
     }
 }
 
-fn compute_column(lines: &Vec<String>, start_idx: usize, end_idx: usize, oper: Operator) -> u64 {
+fn part1(lines: &Vec<String>, start_idx: usize, end_idx: usize, oper: Operator) -> u64 {
     let nums = lines.iter().map(|line| {
-        line[start_idx..(min(line.len(), end_idx))]
+        line[start_idx..(cmp::min(line.len(), end_idx))]
             .trim()
             .parse::<u64>()
             .unwrap()
@@ -32,7 +32,40 @@ fn compute_column(lines: &Vec<String>, start_idx: usize, end_idx: usize, oper: O
     }
 }
 
-fn part1(filename: &str) {
+fn part2(lines: &Vec<String>, start_idx: usize, end_idx: usize, oper: Operator) -> u64 {
+    // For part 2, we go right to left from end_idx-1 to start_idx
+    let mut res = match oper {
+        Operator::Sum => 0,
+        Operator::Product => 1,
+    };
+
+    for i in (start_idx..end_idx).rev() {
+        let mut num_str = String::new();
+        for line in lines {
+            if i >= line.len() || line.chars().nth(i).unwrap() == ' ' {
+                continue;
+            }
+            num_str.push(line.chars().nth(i).unwrap());
+        }
+        let num: u64 = num_str.parse().unwrap();
+
+        match oper {
+            Operator::Sum => {
+                res += num;
+            }
+            Operator::Product => {
+                res *= num;
+            }
+        }
+    }
+
+    return res;
+}
+
+fn runner<F>(filename: &str, column_computer: F)
+where
+    F: Fn(&Vec<String>, usize, usize, Operator) -> u64,
+{
     // Slurp up file contents into vec of strings so we can parse the last one
     let mut lines = BufReader::new(File::open(filename).unwrap())
         .lines()
@@ -61,7 +94,7 @@ fn part1(filename: &str) {
 
         if c != ' ' {
             // Found a character, column is done
-            total += compute_column(&lines, start_idx, i - 1, oper);
+            total += column_computer(&lines, start_idx, i - 1, oper);
 
             oper = Operator::from(c);
             start_idx = i;
@@ -69,11 +102,12 @@ fn part1(filename: &str) {
     }
 
     // Fencepost
-    total += compute_column(&lines, start_idx, max_len, oper);
+    total += column_computer(&lines, start_idx, max_len, oper);
 
     println!("total: {}", total);
 }
 
 pub fn day6(filename: &str) {
-    part1(filename);
+    runner(filename, part1);
+    runner(filename, part2);
 }
