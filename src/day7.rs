227cfa3529599b7ff558c4d5fd5cd27b1b3b519f
Sarang Joshi <sarangj@msn.com>
2025-12-31 18:15:44 -0600

    Finish day 7

--- src/day7.rs
+++ src/day7.rs
@@ -4,7 +4,7 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-pub fn day7(filename: &str) {
+fn part1(filename: &str) {
     let lines = BufReader::new(File::open(filename).unwrap())
         .lines()
         .map(|l| l.unwrap());
@@ -39,4 +39,47 @@ pub fn day7(filename: &str) {
     }
 
     println!("total: {}", tachyon_count);
+}
+
+fn part2(filename: &str) {
+    let lines: Vec<String> = BufReader::new(File::open(filename).unwrap())
+        .lines()
+        .map(|l| l.unwrap())
+        .collect();
+
+    let width = lines[0].len();
+
+    // At every line, the sum of each element in this
+    let mut paths = vec![0_u64; width]; //).map(|_| 0_u64).collect();
+
+    for line in &lines {
+        let mut new_paths = vec![0_u64; width];
+        for (i, c) in line.char_indices() {
+            if c == 'S' {
+                new_paths[i] = 1;
+                break;
+            }
+
+            if paths[i] > 0 {
+                if c == '^' {
+                    if i > 0 {
+                        new_paths[i - 1] += paths[i];
+                    }
+                    if i < line.len() - 1 {
+                        new_paths[i + 1] += paths[i];
+                    }
+                } else {
+                    new_paths[i] += paths[i];
+                }
+            }
+        }
+        paths = new_paths;
+    }
+
+    println!("total: {}", paths.iter().sum::<u64>());
+}
+
+pub fn day7(filename: &str) {
+    part1(filename);
+    part2(filename);
 }
