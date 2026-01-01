ab369ed5f7d030e29932ae111fe59b847cdf6b96
Sarang Joshi <sarangj@msn.com>
2026-01-01 03:48:51 -0600

    Day 8 in progress,

--- src/day8.rs
+++ src/day8.rs
@@ -1,10 +1,12 @@
 use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, HashSet, hash_set},
     fmt::Debug,
     fs::File,
     io::{BufRead, BufReader},
 };
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 struct Point(i64, i64, i64);
 
 impl Point {
@@ -16,12 +18,47 @@ impl Point {
             parts.next().unwrap().parse::<i64>().unwrap(),
         );
     }
+}
 
-    fn distance(self: &Self, other: &Point) -> f64 {
-        (((self.0 - other.0).pow(2) + (self.1 - other.1).pow(2) + (self.2 - other.2).pow(2)) as f64)
+#[derive(Debug)]
+struct PointPair<'a>(&'a Point, &'a Point);
+
+impl PointPair<'_> {
+    fn distance(self: &Self) -> f64 {
+        (((self.0.0 - self.1.0).pow(2)
+            + (self.0.1 - self.1.1).pow(2)
+            + (self.0.2 - self.1.2).pow(2)) as f64)
             .sqrt()
     }
 }
+
+impl Ord for PointPair<'_> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        // Backwards because we want reverse ordering for the max BinaryHeap
+        // other.distance().total_cmp(&self.distance())
+        self.distance().total_cmp(&other.distance())
+
+        // if distance < 0_f64 {
+        //     std::cmp::Ordering::Less
+        // } else if distance > 0_f64 {
+        //     std::cmp::Ord
+        // }
+    }
+}
+
+impl PartialOrd for PointPair<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for PointPair<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.distance() == other.distance()
+    }
+}
+
+impl Eq for PointPair<'_> {}
 
 fn part1(filename: &str) {
     let points: Vec<Point> = BufReader::new(File::open(filename).unwrap())
@@ -29,20 +66,39 @@ fn part1(filename: &str) {
         .map(|l| Point::from(&l.unwrap()))
         .collect();
 
-    // Find the closest two points
-    let mut best: (f64, (&Point, &Point)) = (-1_f64, (&Point(0, 0, 0), &Point(0, 0, 0)));
+    let mut best_pairs = BinaryHeap::new();
+
+    // Collect points by pairs
     for i in 0..points.len() {
         for j in (i + 1)..points.len() {
-            let distance = points[i].distance(&points[j]);
-            if best.0 < 0_f64 || distance < best.0 {
-                // new best
-                best.0 = distance;
-                best.1 = (&points[i], &points[j]);
-            }
+            best_pairs.push(Reverse(PointPair(&points[i], &points[j])));
         }
     }
 
-    println!("best {:?}, {:?}", best.1.0, best.1.1);
+    // Now we can start building circuits
+    // TODO: start circuits with all points to begin with, and merge in the for loop instead of just
+    // adding
+    let mut circuits: Vec<HashSet<&Point>> = Vec::new();
+
+    for _ in 0..3 {
+        let best = best_pairs.pop().unwrap().0;
+        println!("best {:?}", best);
+
+        let mut inserted = false;
+        for c in &mut circuits {
+            if c.contains(best.0) || c.contains(best.1) {
+                c.insert(best.0);
+                c.insert(best.1);
+                inserted = true;
+                break;
+            }
+        }
+        if !inserted {
+            circuits.push(HashSet::from([best.0, best.1]));
+        }
+
+        println!("circuits: {:?}", circuits);
+    }
 }
 
 pub fn day8(filename: &str) {
